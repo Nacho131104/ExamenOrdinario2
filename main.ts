496cdc2b4133df9114665b17c40f344b2add4210
Nacho131104 <nacholopezamat@gmail.com>
2025-06-25 17:05:56 +0200

    commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { schema } from "./schema.ts";
+import { MongoClient } from "mongodb";
+//import { RestaurantModel } from "./types.ts";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolvers.ts";
+
+const MONGO_URL = "mongodb+srv://ilopeza8:1234@cluster0.vbb5s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+if (!MONGO_URL) {
+  throw new Error("MONGO_URL is not defined");
+}
+
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
+
+const mongoDB = mongoClient.db("BaseFinal");
+const ResturantsCollection =
+  mongoDB.collection("Restaurantes");
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  context: async () => ({ ResturantsCollection }),
+});
+
+console.info(`Server ready at ${url}`);

a6bbafc100a40c7f3e265e811a1ef84a42f42a52
Nacho131104 <nacholopezamat@gmail.com>
2025-06-25 19:01:09 +0200

    commi

--- resolvers.ts
+++ resolvers.ts
@@ -1,5 +1,20 @@
+import { GraphQLError } from "graphql"
+import { RestauranteModel } from "./types.ts";
+import { Collection } from "mongodb";
+import { ObjectId } from "mongodb";
+
+type Context = {
+  ResturantsCollection: Collection<RestauranteModel>;
+};
+
 export const resolvers ={
     Query :{
-        DecirHola:() => {return "Hola"}
+        getRestaurant:async(_:unknown, {id}:{id:string},ctx: Context): Promise<RestauranteModel|null> =>{
+            return await ctx.ResturantsCollection.findOne({_id:new ObjectId(id)});
+        },
+
+        getRestaurants:async(_:unknown, {city}:{city: string},ctx:Context): Promise<RestauranteModel[]> =>{
+            return await ctx.ResturantsCollection.find({city}).toArray(); 
+        }
     }
 }
---
+++ types.ts
@@ -0,0 +1,28 @@
+
+import { OptionalId } from "mongodb";
+
+
+export type RestauranteModel =OptionalId<{
+  name: string;                                        
+  address: string;
+  city: string;
+  country: string;
+  phone: string;
+  latitude: string;
+  longitude: string;
+}>                                                             
+
+
+
+//obtener hora y minuto
+export type APIworldtime ={
+    hour: number,
+    minute: number
+}
+
+
+//obtener temperatura
+export type APIweather ={
+    temp: number
+}
+

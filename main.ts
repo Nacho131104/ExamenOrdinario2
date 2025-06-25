f7979cadf6f247aae4aa2274428a494a54d6c550
Nacho131104 <nacholopezamat@gmail.com>
2025-06-25 17:20:16 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { schema } from "./schema.ts";
 import { MongoClient } from "mongodb";
-//import { RestaurantModel } from "./types.ts";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.ts";
 

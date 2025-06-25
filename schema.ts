dba85830439abacdca33769882ff84130a6a1358
Nacho131104 <nacholopezamat@gmail.com>
2025-06-25 17:28:14 +0200

    Update schema.ts

--- schema.ts
+++ schema.ts
@@ -1,5 +1,22 @@
 export const schema = `#graphql
-    type Query {
-        DecirHola: String!
+    type Restaurant {
+        id: ID!
+        name: String!
+        address: String!
+        phone: String!
+        temperature: Int!
+        local_time: String!
+        
     }
+    type Query {
+        getRestaurant(id: ID!): Restaurant
+        getRestaurants(city: String!): [Restaurant!]!
+
+    }
+    type Mutation {
+        addRestaurant(name: String!, address: String!, city: String!, phone: String!): Restaurant
+        deleteRestaurant(id: ID!): Boolean!
+
+    }
+
 `;
